67b1036912f08763d639cc1d8f2c1e3d72a2aa4f
Josue Diaz <jdiaz77711@gmail.com>
2020-08-03 22:26:08 +0100

    add review check if there is already a review && add utilities to handle 404 errors and non 404 errors

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -1,36 +1,77 @@
 <template>
     <div>
-        <div v-if="loading">Loading...</div>
-        <div v-else>
-            <div v-if="alreadyReviewed">
-                <h3>You've already left a review for this booking!</h3>
+        <div v-if="error" class="row">
+            Unknown error has occured, please try again later!
+        </div>
+        <div v-else class="row">
+            <div :class="[{ 'col-md-4': twoColumns }, { 'd-none': oneColumn }]">
+                <div class="card">
+                    <div class="card-body">
+                        <div v-if="loading">Loading...</div>
+                        <div v-if="hasBooking">
+                            <p>
+                                Stayed at
+                                <router-link
+                                    :to="{
+                                        name: 'bookable',
+                                        params: {
+                                            id: booking.bookable.bookable_id
+                                        }
+                                    }"
+                                    >{{ booking.bookable.title }}</router-link
+                                >
+                            </p>
+                            <p>From {{ booking.from }} to {{ booking.to }}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div v-else>
-                <div class="form-group">
-                    <label class="text-muted"
-                        >Select the star rating (1 is worst - 5 is best)</label
-                    >
-                    <star-rating class="fa-3x" v-model="review.rating" />
+            <div
+                :class="[
+                    { 'col-md-8': twoColumns },
+                    { 'col-md-12': oneColumn }
+                ]"
+            >
+                <div v-if="loading">Loading...</div>
+                <div v-else>
+                    <div v-if="alreadyReviewed">
+                        <h3>You've already left a review for this booking!</h3>
+                    </div>
+                    <div v-else>
+                        <div class="form-group">
+                            <label class="text-muted"
+                                >Select the star rating (1 is worst - 5 is
+                                best)</label
+                            >
+                            <star-rating
+                                class="fa-3x"
+                                v-model="review.rating"
+                            />
+                        </div>
+                        <div class="form-group">
+                            <label for="content" class="text-muted"
+                                >Describe your experience with</label
+                            >
+                            <textarea
+                                name="content"
+                                cols="30"
+                                rows="10"
+                                class="form-control"
+                                v-model="review.content"
+                            ></textarea>
+                        </div>
+                        <button class="btn btn-lg btn-primary btn-block">
+                            Submit
+                        </button>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="content" class="text-muted"
-                        >Describe your experience with</label
-                    >
-                    <textarea
-                        name="content"
-                        cols="30"
-                        rows="10"
-                        class="form-control"
-                        v-model="review.content"
-                    ></textarea>
-                </div>
-                <button class="btn btn-lg btn-primary btn-block">Submit</button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import { is404 } from "./../shared/utils/response";
 export default {
     name: "Review",
     data() {
@@ -41,7 +82,8 @@ export default {
             },
             existingReview: null,
             loading: false,
-            booking: null
+            booking: null,
+            error: false
         };
     },
     created() {
@@ -53,28 +95,45 @@ export default {
                 this.existingReview = response.data.data;
             })
             .catch(err => {
-                if (
-                    err.response &&
-                    err.response.status &&
-                    404 === err.response.status
-                ) {
+                if (is404(err)) {
+                    // 2. Fetch a booking by a review key
                     return axios
                         .get(`/api/booking-by-review/${this.$route.params.id}`)
                         .then(response => {
                             this.booking = response.data.data;
+                        })
+                        .catch(err => {
+                            // is404(err) ? {} : (this.error = true);
+                            this.error = !is404(err);
+                            // if (!is404(err)) {
+                            //     this.error = true;
+                            // }
                         });
                 }
+
+                this.error = true;
             })
             .then(response => {
                 console.log(this.booking);
                 this.loading = false;
             });
-        // 2. Fetch a booking by a review key
         // 3. Store the review
     },
     computed: {
         alreadyReviewed() {
+            return this.hasReview || !this.booking;
+        },
+        hasReview() {
             return this.existingReview !== null;
+        },
+        hasBooking() {
+            return this.booking !== null;
+        },
+        oneColumn() {
+            return !this.loading && this.alreadyReviewed;
+        },
+        twoColumns() {
+            return this.loading || !this.alreadyReviewed;
         }
     }
 };
